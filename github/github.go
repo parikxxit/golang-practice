f64c115c8f9ba7a3b9f39931ad4035dd3bb0ef45
parikxxit <singhp154154@gmail.com>
2023-04-08 12:54:06 +0530

    fixed errors

--- github/github.go
+++ github/github.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	resp, err := http.Get("https:/api.github.com/user/parikxxit")
+	resp, err := http.Get("https://api.github.com/users/parikxxit")
 	if err != nil {
 		log.Fatalf("error: %s", err)
 	}
 
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("error := %s", resp.StatusCode)
+	}
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		log.Fatalf("error: cant copy %s", err)
+	}
 }
